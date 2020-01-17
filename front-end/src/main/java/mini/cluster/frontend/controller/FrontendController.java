9df1c7f43003d4577719765339187a9d01db4fc1
Peter <pesegal@gmail.com>
2020-01-16 18:05:09 -0800

    Hook up front-end ms to use Consul K/V

--- front-end/src/main/java/mini/cluster/frontend/controller/FrontendController.java
+++ front-end/src/main/java/mini/cluster/frontend/controller/FrontendController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.naming.ServiceUnavailableException;
 
 @Controller
+@RefreshScope
 public class FrontendController {
     Logger logger = LoggerFactory.getLogger(FrontendController.class);
 
