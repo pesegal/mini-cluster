e4f56ae9629ec0a422368d09bd30ca86f28df3dd
Peter <pesegal@gmail.com>
2020-01-28 18:05:36 -0800

    Secrets MGMT: add postgres instance to cluster

---
+++ configRepo/dbConfig/init.sql
@@ -0,0 +1,6 @@
+
+-- This is executed upon initialization of the database docker image
+-- (note will not run again if data dir exists: check volumes if you have trouble)
+-- docker inspect postgres-dev | grep volume
+-- docker volume rm `volume-id`
+create DATABASE count_service;
