1a8fce52b17873f1b8c6f2340e413112ca05376e
Peter <pesegal@gmail.com>
2020-01-30 21:43:46 -0800

    Secrets MGMT: vault config

--- configRepo/vaultConfig/vault.hcl
+++ configRepo/vaultConfig/vault.hcl
@@ -2,3 +2,6 @@ storage "consul" {
   address = "http://consul:8500"
   path = "vault"
 }
+
+
+// TODO: Add dynamic database configuration
---
+++ configRepo/vaultConfig/vault_config_secrets.sh
@@ -0,0 +1,15 @@
+VAULT_TOKEN="vault_dev"
+
+# Enable Secrets Engine
+curl --header "X-Vault-Token: $VAULT_TOKEN" \
+     --request POST \
+     --data @enable_database_secrets_engine.json \
+     http://localhost:8200/v1/sys/mounts/database
+
+
+
+# Execute API commands to set up postgres database engine via HTTP
+curl --header "X-Vault-Token: $VAULT_TOKEN" \
+     --request POST \
+     --data @postgres_engine.json \
+     http://localhost:8200/v1/database/config/count-service
